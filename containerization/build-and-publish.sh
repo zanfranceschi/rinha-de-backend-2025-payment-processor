739bff5b0686e50f268e6ff0884c4c29938abd6e
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2025-07-07 10:21:00 -0300

    add timestamp to tags

--- containerization/build-and-publish.sh
+++ containerization/build-and-publish.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+timestamp=$(date '+%Y%m%d%H%M%S')
+
 docker buildx build --platform linux/amd64 \
-    -t zanfranceschi/payment-processor:amd64 ../src/payment-processor 
-docker push zanfranceschi/payment-processor:amd64
+    -t zanfranceschi/payment-processor:amd64-$timestamp ../src/payment-processor 
+docker push zanfranceschi/payment-processor:amd64-$timestamp
 
 docker buildx build --platform linux/arm64 \
-    -t zanfranceschi/payment-processor:arm64 ../src/payment-processor
-docker push zanfranceschi/payment-processor:arm64
+    -t zanfranceschi/payment-processor:arm64-$timestamp ../src/payment-processor
+docker push zanfranceschi/payment-processor:arm64-$timestamp
