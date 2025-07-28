fbbfab1504e6f86a2386e8cc7e6afee60bfcb47c
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2025-07-28 11:33:17 -0300

    AOT

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 ## Payment Processor
 
 Este repositório contém o código dos Payment Processors usado na [terceira edição da Rinha de Backend](https://github.com/zanfranceschi/rinha-de-backend-2025).
+
+
+### Teste de Stress
+
+```shell
+cd ./src/payment-processor/containerization
+
+docker compose up
+
+cd ../
+
+k6 run -e TEST_DURATION=120s -e MAX_REQUESTS=700 -e TOKEN=123 stress-test.js
+```
+
--- containerization/build-and-publish.sh
+++ containerization/build-and-publish.sh
@@ -3,9 +3,20 @@
 timestamp=$(date '+%Y%m%d%H%M%S')
 
 docker buildx build --platform linux/amd64 \
-    -t zanfranceschi/payment-processor:amd64-$timestamp ../src/payment-processor 
+    -t zanfranceschi/payment-processor:amd64-$timestamp \
+    -t payment-processor:amd64-$timestamp ../src/payment-processor 
 docker push zanfranceschi/payment-processor:amd64-$timestamp
 
 docker buildx build --platform linux/arm64 \
     -t zanfranceschi/payment-processor:arm64-$timestamp ../src/payment-processor
 docker push zanfranceschi/payment-processor:arm64-$timestamp
+
+echo "=============="
+echo "     tag      "
+echo "=============="
+echo $timestamp
+echo "=============="
+
+# latest:
+#   arm64-20250728011553 
+#   amd64-20250728011553
